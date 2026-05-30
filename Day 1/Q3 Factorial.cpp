314a03b6ea4733086316771c0154b1f53ab2436a
Kartikey Verma <kartikeyverma0110@Kartikeys-MacBook-Air.local>
2026-05-31 00:34:18 +0530

    Updated Q3 Factorial  program

--- Day 1/Q3 Factorial.cpp
+++ Day 1/Q3 Factorial.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 int main()
 {
-    int n,fact=0,i;
+    int n,fact=1,i;
     cout<<"Enter a Number:";
     cin>>n;
     for(i=n;i>0;i--)
     {
-        fact=fact+i;
+        fact=fact*i;
     }
     cout<<"Factorial Of "<<n<<" = "<<fact;
     return 0;
